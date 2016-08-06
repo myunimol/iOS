04de95b3b3da33a2cbd644852a4a0d91219d2bbc
giograno <granogiovanni90@gmail.com>
2016-08-06 12:09:10 +0200

    First draft for exam enrollment

--- MyUnimol/BookExamController.swift
+++ MyUnimol/BookExamController.swift
@@ -20,16 +20,57 @@ class BookExamController: UIViewController {
     
     @IBOutlet weak var date: UILabel!
     
+    @IBOutlet weak var enrollButton: UIButton!
+    
+    @IBOutlet weak var okButton: UIImageView!
+    
+    @IBAction func bookExam(sender: AnyObject) {
+        self.enrollButton.enabled = false
+        
+        let alertController = UIAlertController(title: "😎 Yeeahhh", message: "Sei stato prenotato all'esame di \(self.exam!.name!)!\n\nOra fila a studiare 😪", preferredStyle: .Alert)
+        alertController.addAction((UIAlertAction(title: "OK", style: .Default, handler: {(action) -> Void in
+        })))
+        self.presentViewController(alertController, animated: true) {
+            self.okButton.hidden     = false
+            self.enrollButton.hidden = true
+        }
+    }
+    
+    func appoggio () {
+        self.enrollButton.enabled = false
+        
+        Utils.progressBarDisplayer(self, msg: LoadSentences.getSentence(), indicator: true)
+        SessionExam.enroll(exam!.id!) { api, error in
+            guard error == nil else {
+                Utils.removeProgressBar(self)
+                Utils.displayAlert(self, title: "😱 Oooops!!", message: "Per qualche strano motivo la prenotazione non è andata a buon fine 😅")
+                self.enrollButton.enabled = true
+                return
+            } // end error
+            
+            Utils.removeProgressBar(self)
+            let alertController = UIAlertController(title: "😎 Yeeahhh", message: "Sei stato prenotato all'esame di \(self.exam!.name!)!\n\nOra fila a studiare 😪", preferredStyle: .Alert)
+            alertController.addAction((UIAlertAction(title: "OK", style: .Default, handler: {(action) -> Void in
+            })))
+            self.presentViewController(alertController, animated: true) {
+                self.okButton.hidden     = false
+                self.enrollButton.hidden = true
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureNavigationController()
+        self.enrollButton.hidden    = false
+        self.okButton.hidden        = true
         self.examName.text          = exam!.name
         self.professorName.text     = exam!.professor
         self.date.attributedText    = self.attributedExamDateString()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    override func viewWillDisappear(animated: Bool) {
+        // here I have to put an asyncronous call to refresh the API for available exams
     }
     
     func configureNavigationController() {
